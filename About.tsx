4df13c88e37268dd41278a32d58ea278bd004a94
SADIA <sadiabaloch565@gmail.com>
2024-10-15 21:57:25 +0500

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>lorem ipsum dolor sit amet, consecteture adipiscing elit.</p>
+        </div>
+    );
+};
+export default About;
---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  );
+};
+export default App;
